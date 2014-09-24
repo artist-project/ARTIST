b3ec281b181b3fc5453686fae77457e230b6e791
Martin Fleck <fleck@big.tuwien.ac.at>
2014-09-24 18:48:46 +0200

    Changed Copyright to ARTIST conform message

--- source/Tooling/post-migration/nfrvt/eu.artist.postmigration.nfrvt.eval.run/src/eu/artist/postmigration/nfrvt/eval/run/ui/launch/ModelSelectionTab.java
+++ source/Tooling/post-migration/nfrvt/eu.artist.postmigration.nfrvt.eval.run/src/eu/artist/postmigration/nfrvt/eval/run/ui/launch/ModelSelectionTab.java
@@ -1,14 +1,15 @@
-/**
- * Copyright (c) 2013, 2014 Vienna University of Technology.
- * All rights reserved. This program and the accompanying materials are made 
- * available under the terms of the Eclipse Public License v1.0 which accompanies 
- * this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
- * 
+/*******************************************************************************
+ * Copyright (c) 2014 Vienna University of Technology.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
  * Contributors:
- * Philip Langer - initial API and implementation
- * Tanja Mayerhofer - implementation
- * Martin Fleck - extended for the use in ARTIST.
- */
+ * Martin Fleck (Vienna University of Technology) - initial API and implementation
+ *
+ * Initially developed in the context of ARTIST EU project www.artist-project.eu
+ *******************************************************************************/
 package eu.artist.postmigration.nfrvt.eval.run.ui.launch;
 
 import java.util.ArrayList;
