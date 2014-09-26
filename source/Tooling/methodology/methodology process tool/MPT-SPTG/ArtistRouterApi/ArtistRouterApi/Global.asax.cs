e75bdb155ee9ba6db21709aa2f9371b77de74633
Bram Pellens <bpellens@gmail.com>
2014-09-26 15:13:08 +0200

    Updated copyright notices to Spikes Tools

--- source/Tooling/methodology/methodology process tool/MPT-SPTG/ArtistRouterApi/ArtistRouterApi/Global.asax.cs
+++ source/Tooling/methodology/methodology process tool/MPT-SPTG/ArtistRouterApi/ArtistRouterApi/Global.asax.cs
@@ -1,5 +1,4 @@
-﻿
-/*
+﻿/*
  * The MIT License (MIT)
  * Copyright (c) 2014 Spikes N.V.
  *
@@ -20,6 +19,8 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
+ *
+ * Initially developed in the context of ARTIST EU project www.artist-project.eu
  * */
 
 using System;
